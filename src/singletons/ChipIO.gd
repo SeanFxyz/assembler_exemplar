045c0fc2b4a41b4bd0ba8ea3c8619a6e39c7b50a
BuildTools <sean_m_fitzgerald@tutanota.com>
2021-10-06 10:25:33 -0700

    Added first dictionary to ChipIO.gd

--- src/singletons/ChipIO.gd
+++ src/singletons/ChipIO.gd
@@ -1,16 +1,8 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var nand := {
+	0b00: [1],
+	0b01: [1],
+	0b10: [1],
+	0b11: [0]
+}
