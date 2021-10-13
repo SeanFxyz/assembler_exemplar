0d6a0b7ec7bcc07b9f0a0cb239d36f8901c828f8
BuildTools <sean_m_fitzgerald@tutanota.com>
2021-10-13 15:59:02 -0700

    updated ChipIO.gd for style comformity

--- src/singletons/ChipIO.gd
+++ src/singletons/ChipIO.gd
@@ -4,12 +4,12 @@ var nand := {
 	0b00: [1],
 	0b01: [1],
 	0b10: [1],
-	0b11: [0]
+	0b11: [0],
 }
 
 # Find chip dictionaries by name
 var chips := {
-	"nand": nand
+	"nand": nand,
 }
 
 func find_chip(name: String) -> Dictionary:
