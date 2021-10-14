8b7fe68c290be26a39d0319103d240daf85ff3b5
BuildTools <sean_m_fitzgerald@tutanota.com>
2021-10-13 22:30:53 -0700

    More work on PlayerData.gd

--- src/misc/PlayerData.gd
+++ src/misc/PlayerData.gd
@@ -2,8 +2,10 @@
 # player data. These can be used in whatever Scene/Node will be responsible
 # for loading player solution data when opening a level.
 
+# load_savfile(file_name: String) -> Dictionary
+# loads the data in a save file
 func load_savfile(file_name: String) -> Dictionary:
-	
+
 	var sav_data := {}
 
 	var file := File.new()
@@ -26,6 +28,9 @@ func load_savfile(file_name: String) -> Dictionary:
 
 	return sav_data
 
+
+# load_recfile(file_name: String) -> Array
+# loads the data in a recovery file into an Array of the file's lines
 func load_recfile(file_name: String) -> Array:
 	var rec_data := []
 
@@ -44,6 +49,9 @@ func load_recfile(file_name: String) -> Array:
 
 	return rec_data
 
+
+# write_savfile(file_name: String, level_data: Dictionary) -> int
+# writes data from an appropriately formatted Dictionary to a savefile
 func write_savfile(file_name: String, level_data: Dictionary) -> int:
 
 	var file := File.new()
@@ -60,7 +68,48 @@ func write_savfile(file_name: String, level_data: Dictionary) -> int:
 
 	return OK
 
-func update_from_rec(sav_data: Dictionary, rec_data: Array):
+
+# update_from_rec(save_data: Dictionary, rec_data: Array) -> void
+# Update a save data dictionary from an array of recovery file lines.
+# returns an error code
+func update_from_rec(sav_data: Dictionary, rec_data: Array) -> void:
 
 	for line in rec_data:
-		pass
+		var rec_op = parse_json(line)
+		var op_type = rec_op["op"]
+
+		if   op_type == "MOVE_INPUT":
+			return move_input(sav_data, rec_op)
+		elif op_type == "MOVE_OUTPUT":
+			return move_output(sav_data, rec_op)
+		elif op_type == "ADD_CHIP":
+			return add_chip(sav_data, rec_op)
+		elif op_type == "DELETE_CHIP":
+			return delete_chip(sav_data, rec_op)
+		elif op_type == "ADD_WIRE":
+			return add_wire(sav_data, rec_op)
+		elif op_type == "DELETE_WIRE":
+			return delete_wire(sav_data, rec_op)
+		elif op_type == "MERGE_WIRE":
+			return merge_wire(sav_data, rec_op)
+		elif op_type == "SPLIT_WIRE":
+			return split_wire(sav_data, rec_op)
+		elif op_type == "RENAME_SOLUTION":
+			return rename_solution(sav_data, rec_op)
+		else:
+			return FAILED
+
+
+# move_input(sav_data: Dictionary, rec_op: Dictionary) -> int
+# move an input's x/y position in sav_data as specified by rec_op,
+# returning error code
+func move_input(sav_data: Dictionary, rec_op: Dictionary) -> int:
+	# Get the sub-dictionary for the particular solution being modified
+	var solution : Dictionary = sav_data["solutions"][rec_op["solutions"]]
+
+	# Get the sub-dictionary for inputs
+	var inputs : Dictionary = solution["inputs"]
+
+	inputs[rec_op["changed_input"]] = rec_op["pos"]
+
+	return OK
