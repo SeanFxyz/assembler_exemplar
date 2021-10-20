5ce25cf7e144f47de6706c86efbd8ff692f86709
BuildTools <sean_m_fitzgerald@tutanota.com>
2021-10-19 21:16:24 -0700

    Added a descriptive comment to top of ChipIO

--- src/singletons/ChipIO.gd
+++ src/singletons/ChipIO.gd
@@ -1,3 +1,10 @@
+# The `ChipIO` Singleton include pre-populated dictionaries mapping chip
+# inputs to the corresponding outputs, which is used by nodes to
+# simulate player-created chips and verify them during testing.
+# Since more than one part of the program needs to know the correct output of
+# a chip given a particular input, this singleton centralizes that
+# functionality.
+
 extends Node
 
 # Chip name: Nand
@@ -13,11 +20,19 @@ var Nand := {
 	0b11: { "out": 0 },
 }
 
+# Chip name: Not
+# Inputs:    in
+# Outputs:   out
+# Function:  If in=0 then out=1 else out=0.
 var Not := {
 	0b0: { "out": 1 },
 	0b1: { "out": 0 },
 }
 
+# Chip name: And
+# Inputs:    a, b
+# Outputs:   out
+# Function:  If a=b=1 then out=1 else out=0.
 var And := {
 	0b00: { "out": 0 },
 	0b01: { "out": 0 },
@@ -25,6 +40,10 @@ var And := {
 	0b11: { "out": 1 },
 }
 
+# Chip name: Or
+# Inputs:    a, b
+# Outputs:   out
+# Function:  If a=b=0 then out=0 else out=1.
 var Or := {
 	0b00: { "out": 0 },
 	0b01: { "out": 1 },
@@ -32,6 +51,10 @@ var Or := {
 	0b11: { "out": 1 },
 }
 
+# Chip name: Xor
+# Inputs:    a, b
+# Outputs:   out
+# Function:  If a!=b then out=1 else out=0.
 var Xor := {
 	0b00: { "out": 0 },
 	0b01: { "out": 1 },
@@ -39,6 +62,10 @@ var Xor := {
 	0b11: { "out": 0 },
 }
 
+# Chip name: Mux
+# Inputs:    a, b, sel
+# Outputs:   out
+# Function:  If sel=0 then out=a else out=b.
 var Mux := {
 	0b000: { "out": 0 },
 	0b010: { "out": 0 },
