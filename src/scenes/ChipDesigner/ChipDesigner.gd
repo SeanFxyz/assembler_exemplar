a84d25a32e471ffff8223efe4b1de2c84f269f0a
henatic <hm772@humboldt.edu>
2021-10-31 00:49:11 -0700

    Playing and practicing with creating a canvas grid.

---
+++ src/scenes/ChipDesigner/ChipDesigner.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Create grid to place chips
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var tile_size = 64
+var inputs = {"right": Vector2.RIGHT,
+			"left": Vector2.LEFT,
+			"up": Vector2.UP,
+			"down": Vector2.DOWN}
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
