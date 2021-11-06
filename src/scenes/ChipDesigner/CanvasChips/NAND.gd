2ee1b47acb3ef2993a572a90a83768e2881f205f
Max <map209@humboldt.edu>
2021-11-06 11:58:48 -0700

    Messy, but NAND gates can now be moved within a scene.

--- src/scenes/ChipDesigner/CanvasChips/NAND.gd
+++ src/scenes/ChipDesigner/CanvasChips/NAND.gd
@@ -8,7 +8,26 @@ var inputs = {
 func _ready():
 	pass # Replace with function body.
 
+var prev_mouse_position = Vector2()
+var is_dragged = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Drag(_viewport, event, _shape_idx):
+	# Allow dragging if user clicks CollisionShape2D
+	if event.is_action_pressed("ui_select"):
+		print(event)
+		get_tree().set_input_as_handled()
+		prev_mouse_position = event.position
+		is_dragged = true
+		
+func _input(event):
+	# Disable dragging if click is released
+	if not is_dragged:
+		return
+	
+	if event.is_action_released("ui_select"):
+		prev_mouse_position = Vector2()
+		is_dragged = false
+	
+	if is_dragged and event is InputEventMouseMotion:
+		position += event.position - prev_mouse_position
+		prev_mouse_position = event.position
