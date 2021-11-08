6e2c0d5d67335ef52d5d5e554d30bb707710f775
Max <map209@humboldt.edu>
2021-11-08 09:54:36 -0800

    Set the "zoom" values to be more reasonable.

--- src/scenes/ChipDesigner/CameraController.gd
+++ src/scenes/ChipDesigner/CameraController.gd
@@ -4,10 +4,10 @@ extends Camera2D
 var zoom_cur := 1.0
 
 # Minimum zoom level
-var zoom_min := 0.1
+var zoom_min := 1.0
 
 # Maximum zoom level
-var zoom_max := 2.0
+var zoom_max := 5.0
 
 # Zoom increment
 var zoom_inc := 0.1
