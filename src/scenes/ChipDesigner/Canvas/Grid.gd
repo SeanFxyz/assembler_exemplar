eb4d6fee748ed1ffa5ad8675d8926c83fdc47c16
Max <map209@humboldt.edu>
2021-11-08 09:46:34 -0800

    Grid displays properly.

--- src/scenes/ChipDesigner/Canvas/Grid.gd
+++ src/scenes/ChipDesigner/Canvas/Grid.gd
@@ -1,22 +1,26 @@
 extends Node2D
 
-export var lower_y_bound = -10000
 export var lower_x_bound = -10000
 export var upper_x_bound = 10000
+export var lower_y_bound = -10000
 export var upper_y_bound = 10000 
 export var snap_inc = 50
 export var grid_line_color = Color(.5, .5, .5, 1)
-onready var view_rect = get_viewport().get_visible_rect()
+#onready var view_rect = get_viewport().get_visible_rect()
 
 func _draw():
-	for i in range(lower_y_bound, upper_y_bound, snap_inc):
-		draw_line(Vector2(lower_x_bound, lower_y_bound + i),
-				  Vector2(upper_x_bound, lower_y_bound + i),
+	var i = lower_x_bound
+	while(i <= upper_x_bound):
+		draw_line(Vector2(i, lower_y_bound),
+				  Vector2(i, upper_y_bound),
 				  grid_line_color)
-	for i in range(lower_x_bound, upper_x_bound, snap_inc):
-		draw_line(Vector2(lower_x_bound + i, lower_y_bound),
-				  Vector2(lower_x_bound + i, upper_y_bound),
+		i += snap_inc
+	i = lower_y_bound
+	while(i <= upper_y_bound):
+		draw_line(Vector2(lower_x_bound, i),
+				  Vector2(upper_x_bound, i),
 				  grid_line_color)
+		i += snap_inc
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
