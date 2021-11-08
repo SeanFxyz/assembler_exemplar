2e2a033cb3c6418d9ec9ec7a056a483a9ef3d18f
BuildTools <sean_m_fitzgerald@tutanota.com>
2021-11-07 20:33:44 -0800

    Start work on panning in CameraController

--- src/scenes/ChipDesigner/CameraController.gd
+++ src/scenes/ChipDesigner/CameraController.gd
@@ -12,13 +12,29 @@ var zoom_max := 2.0
 # Zoom increment
 var zoom_inc := 0.1
 
+# Whether the camera is being dragged
+var is_dragged := false
+
+# Previous mouse position
+var prev_mouse_position := Vector2()
+
 func _ready():
 	zoom = Vector2(zoom_cur, zoom_cur)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action("ui_zoom_in"):
 		zoom_cur = max(zoom_min, zoom_cur - zoom_inc)
+		zoom = Vector2(zoom_cur, zoom_cur)
 	elif event.is_action("ui_zoom_out"):
 		zoom_cur = min(zoom_max, zoom_cur + zoom_inc)
-	
-	zoom = Vector2(zoom_cur, zoom_cur)
+		zoom = Vector2(zoom_cur, zoom_cur)
+	elif event.is_action("ui_drag_camera"):
+		if not is_dragged:
+			return
+		elif event is InputEventMouseMotion:
+			position += event.position - prev_mouse_position
+			prev_mouse_position = event.position
+		if event.is_action_released("ui_drag_camera"):
+			prev_mouse_position = Vector2()
+			is_dragged = false
+		
