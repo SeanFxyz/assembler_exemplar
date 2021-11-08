ca72711d2a91bb88930259d7199d4e026a179919
Max <map209@humboldt.edu>
2021-11-07 16:33:20 -0800

    Created basic grid system.

--- src/scenes/ChipDesigner/Canvas/Grid.gd
+++ src/scenes/ChipDesigner/Canvas/Grid.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
+export var snap_inc = 50
+export var grid_line_color = Color(.5, .5, .5, 1)
+onready var view_rect = get_viewport().get_visible_rect()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+func _draw():
+	for i in range(view_rect.position.y, view_rect.end.y, snap_inc):
+		draw_line(Vector2(view_rect.position.x, view_rect.position.y + i),
+				  Vector2(view_rect.end.x, view_rect.position.y + i),
+				  grid_line_color)
+	for i in range(view_rect.position.x, view_rect.end.x, snap_inc):
+		draw_line(Vector2(view_rect.position.x + i, view_rect.position.y),
+				  Vector2(view_rect.position.x + i, view_rect.end.y),
+				  grid_line_color)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
