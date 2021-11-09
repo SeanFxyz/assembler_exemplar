c534358a0103a6e83e9d593105477414e534c98f
Max <map209@humboldt.edu>
2021-11-09 10:56:30 -0800

    Menu items are populated and clickable.

--- src/scenes/ChipDesigner/ChipDesigner.gd
+++ src/scenes/ChipDesigner/ChipDesigner.gd
@@ -2,3 +2,11 @@ extends Control
 
 onready var canvas_switcher := $CanvasSwitcher
 
+func _on_ItemList_item_selected(index):
+	print("item clicked")
+	pass # Replace with function body.
+
+
+func _on_ItemList_nothing_selected():
+	print("nothing selected")
+	pass # Replace with function body.
