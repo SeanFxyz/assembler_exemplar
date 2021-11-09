e452c1d2b5d181e172cb82a3b938a23af8e2ae15
Max <map209@humboldt.edu>
2021-11-09 11:33:26 -0800

    Configuring ItemList signals.

--- src/scenes/ChipDesigner/ChipDesigner.gd
+++ src/scenes/ChipDesigner/ChipDesigner.gd
@@ -1,12 +1,21 @@
 extends Control
 
 onready var canvas_switcher := $CanvasSwitcher
+onready var item_selected = false
+onready var mouse_moved_to_canvas = false
+onready var selected_item
 
 func _on_ItemList_item_selected(index):
 	print("item clicked")
-	pass # Replace with function body.
-
+	item_selected = true
+	selected_item = $ItemListControl/ItemList.get_item_text(index)
+	print(selected_item)
 
 func _on_ItemList_nothing_selected():
 	print("nothing selected")
-	pass # Replace with function body.
+	item_selected = false
+	
+func _on_ItemList_mouse_exited():
+	print("exiting control node housing ItemList")
+	if (item_selected == true):
+		print("Moving " + selected_item + " to screen")
