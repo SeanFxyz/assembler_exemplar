5aff0bf1be61f246528c03ab02a7efef48b3b677
henatic <hm772@humboldt.edu>
2021-11-13 14:14:33 -0800

    Attempted to implement my version of Wires/wire.gd to an extent.

--- src/scenes/ChipDesigner/Wires/wire.gd
+++ src/scenes/ChipDesigner/Wires/wire.gd
@@ -1,16 +1,23 @@
 extends Line2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	connect("input_event", self, "_on_Drag")
+	connect("mouse_entered", self, "_on_mouse_entered")
+	connect("mouse_exited", self, "_on_mouse_exited")
 
+var prev_mouse_position = Vector2()
+var end_mouse_position
+var new_line = Line2D
+var is_dragged = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_mouse_entered(event):
+	prev_mouse_position = add_point(get_local_mouse_position(), 0)
+
+func _on_Drag(event):
+	is_dragged = true
+	end_mouse_position = get_local_mouse_position()
+
+func _on_mouse_exited(event):
+	is_dragged = false
+	new_line = add_point(get_local_mouse_position(), 1)
