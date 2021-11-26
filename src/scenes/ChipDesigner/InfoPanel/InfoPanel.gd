80a36c9d5572ae606de4c7547a7ff05ed9e6aac5
BuildTools <sean_m_fitzgerald@tutanota.com>
2021-11-26 11:19:32 -0800

    stop InfoPanel from crashing at runtime

--- src/scenes/ChipDesigner/InfoPanel/InfoPanel.gd
+++ src/scenes/ChipDesigner/InfoPanel/InfoPanel.gd
@@ -4,15 +4,18 @@ extends Control
 export var head_text   : String setget set_head_text
 export var body_text : String setget set_body_text
 
-onready var head := $Head
-onready var body := $MarkdownLabel
+onready var is_ready := true
+onready var head     := $Head
+onready var body     := $MarkdownLabel
 
 
 func set_head_text(new_value: String) -> void:
 	head_text = new_value
-	head.text = new_value
+	if is_ready:
+		head.text = new_value
 	
 
 func set_body_text(new_value: String) -> void:
 	body_text = new_value
-	body.md_text = new_value
+	if is_ready:
+		body.md_text = new_value
