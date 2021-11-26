2529b6c3f7afe812b0245d5c81d2c8a75c3f62bd
BuildTools <sean_m_fitzgerald@tutanota.com>
2021-11-26 10:54:56 -0800

    Update ChipDesigner UI

---
+++ src/scenes/ChipDesigner/TestControl.gd
@@ -0,0 +1,31 @@
+extends HBoxContainer
+
+signal test_control_pressed(control)
+signal test_control_toggled(control, pressed)
+
+onready var play := $PlayButton
+onready var fast_forward := $FastForwardButton
+
+
+func _on_ResetButton_pressed():
+	emit_signal("test_control_pressed", "reset")
+
+
+func _on_StepBackButton_pressed():
+	emit_signal("test_control_pressed", "step_back")
+
+
+func _on_StepForwardButton_pressed():
+	emit_signal("test_control_pressed", "step_forward")
+
+
+func _on_PlayButton_toggled(button_pressed):
+	if button_pressed and fast_forward.pressed:
+		fast_forward.pressed = false
+	emit_signal("test_control_toggled", "play", button_pressed)
+
+
+func _on_FastForwardButton_toggled(button_pressed):
+	if button_pressed and play.pressed:
+		play.pressed = false
+	emit_signal("test_control_toggled", "fast_forward", button_pressed)
