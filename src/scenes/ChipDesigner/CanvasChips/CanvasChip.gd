50592e6c94145d67df02585d9181b60db33c7fc7
BuildTools <sean_m_fitzgerald@tutanota.com>
2021-11-29 21:25:04 -0800

    Added/Updated CanvasChip-related nodes/scripts

--- src/scenes/ChipDesigner/CanvasChips/CanvasChip.gd
+++ src/scenes/ChipDesigner/CanvasChips/CanvasChip.gd
@@ -6,21 +6,33 @@ class_name CanvasChip
 const snap_inc := CanvasInfo.grid_inc
 
 export var chip_type := ""
-var prev_mouse_position = Vector2()
-var is_dragged = false
+var chip_id : int
+var input_nodes   := {}
+var input_states  := {}
+var output_nodes  := {}
+
+var prev_mouse_position := Vector2()
+var is_dragged := false
 
 
-onready var sprite_rect : Rect2 = $Sprite.get_rect()
+onready var inputs      : Node2D = $Inputs
+onready var outputs     : Node2D = $Outputs
+onready var chip_spec   : ChipSpec = ChipIO.chip_specs[chip_type]
+onready var sprite_rect : Rect2  = $Sprite.get_rect()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if connect("input_event", self, "_on_Drag") != OK:
-		printerr("CanvasChip: failed to connect signal: input_event")
-	if connect("mouse_entered", self, "_on_mouse_entered") != OK:
-		printerr("CanvasChip: failed to connect signal: mouse_entered")
-	if connect("mouse_exited", self, "_on_mouse_exited") != OK:
-		printerr("CanvasChip: failed to connect signal: mouse_exited")
+	if (connect("input_event", self, "_on_Drag") != OK or
+			connect("mouse_entered", self, "_on_mouse_entered") != OK or
+			connect("mouse_exited", self, "_on_mouse_exited") != OK):
+		printerr("CanvasChip: failed to connect signal")
+	
+	for input in inputs.get_children():
+		input_nodes[input.input_name] = input
+		input_states[input.input_name] = false
+	for output in outputs.get_children():
+		output_nodes[output.output_name] = output
 
 
 func _on_Drag(_viewport, event, _shape_idx):
@@ -31,11 +43,14 @@ func _on_Drag(_viewport, event, _shape_idx):
 		is_dragged = true
 		CanvasInfo.chips_dragged += 1
 		
+		
 func _on_mouse_entered():
 	CanvasInfo.entities_hovered += 1
 
+
 func _on_mouse_exited():
 	CanvasInfo.entities_hovered -= 1
+		
 		
 func _input(event):
 	# Disable dragging if click is released
@@ -52,3 +67,55 @@ func _input(event):
 	if is_dragged and event is InputEventMouseMotion:
 		position += (event.position - prev_mouse_position) * CanvasInfo.zoom_cur
 		prev_mouse_position = event.position
+
+
+#func get_input_value(input_name: String) -> int:
+#
+#	return 0
+#
+#
+#func get_output_value(output_name: String) -> int:
+#	var input_values := []
+#
+#	for input_name in chip_spec.input_names:
+#		input_values.append(get_input_value(input_name))
+#
+#	var output_values : Dictionary
+#	output_values = chip_spec.io[chip_spec.format_input(input_values)]
+#
+#	return output_values[output_name]
+
+
+func set_input_state(input_name: String, state: int) -> void:
+	input_states[input_name] = state
+	update_outputs()
+
+
+func update_outputs() -> void:
+	var output_states : Dictionary = (
+		chip_spec.io[chip_spec.format_input(input_states)])
+	
+	for oname in output_states.keys():
+		var owire := get_output_wire(oname)
+		if owire:
+			owire.wire_state = output_states[oname]
+
+
+func get_input_wire(input_name: String) -> Node2D:
+	var overlaps : Array = input_nodes[input_name].get_overlapping_areas()
+	if overlaps.size() > 0:
+		return overlaps[0].wire
+	else:
+		return null
+
+
+func get_output_wire(output_name: String) -> Node2D:
+	var overlaps : Array = output_nodes[output_name].get_overlapping_areas()
+	if overlaps.size() > 0:
+		return overlaps[0].wire
+	else:
+		return null
+		
+
+func input_state_to_string(state: int) -> String:
+	return str(state)
